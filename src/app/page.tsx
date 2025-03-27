d5b8bf58739dfa784d76a0d063e011915651a51b
Mykhailo Chudyk <mchudyk@uni.minerva.edu>
2025-03-27 16:02:22 -0700

    Change position from absolute to relative

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
 import pointer from "./images/pointer.png";
-import ClientAnimatedOval from "@/components/ClientAnimatedOval";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen flex justify-center w-full">
-      <div className="absolute left-1/2 -translate-x-1/2 max-w-[700px] min-w-[700px] h-[500px] flex justify-center items-center">
-        <div className="absolute top-20 left-20">
+    <div className="relative min-h-screen flex justify-center w-full max-w-[1600px]">
+      <div className="relative max-w-[700px] min-w-[700px] h-[600px] flex flex-col p-8">
+        <div className="mt-8">
           <h1 className="text-[93px] font-arsenica italic font-medium">hello<span className="not-italic">!</span></h1>
         </div>
-        <div className="absolute top-40 left-50">
+        <div className="mt-[-65px] ml-30">
           <h1 className="text-[93px] font-arsenica font-medium">it&apos;s</h1>
         </div>
-        <div className="absolute top-70 left-100">
+        <div className="mt-[-40px] ml-80">
           <h1 className="text-[93px] font-arsenica font-medium">Yeva</h1>
         </div>
-        <div className="absolute top-120 left-55">
+        <div className="w-full flex-col items-center mt-10 ml-40">
           <h1 className="text-[31px] font-arsenica font-medium">let me <span className="italic text-[#42367e] cursor-pointer">introduce</span> myself</h1>
-          <Image src={pointer} alt="pointer" width={35} height={35} className="ml-30 cursor-pointer" />
+        </div>
+        <div className="w-full flex-col items-center mt-0 ml-62">
+          <Image src={pointer} alt="pointer" width={35} height={35} className="ml-8 cursor-pointer" />
         </div>
       </div>
     </div>
