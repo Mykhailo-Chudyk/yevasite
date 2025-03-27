ff91d39b33134936b42f49ca70a4a18a7a4a2c07
Mykhailo Chudyk <mchudyk@uni.minerva.edu>
2025-03-27 12:53:02 -0700

    Fix font setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+
+const arsenica = Cormorant_Garamond({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-arsenica",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} ${arsenica.variable} antialiased`}
       >
         <Header />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-
+    <div className="relative min-h-screen">
+      <div className="absolute top-20 left-20">
+        <h1 className="text-[93px] font-arsenica">hello!</h1>
+      </div>
+      <div className="absolute top-32 left-40">
+        <h1 className="text-[93px] font-arsenica">it's</h1>
+      </div>
+      <div className="absolute top-44 left-60">
+        <h1 className="text-[93px] font-arsenica">Yeva</h1>
+      </div>
     </div>
   );
 }
