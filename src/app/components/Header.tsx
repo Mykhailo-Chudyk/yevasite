188cc139bd1355127ba7cbbec112d044440155dc
Mykhailo Chudyk <mchudyk@uni.minerva.edu>
2025-03-27 17:45:06 -0700

    Design improvements

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,7 @@
 import { Arsenal } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+import yevayevtikhova from "../images/yevayevtikhova.png";
 
 const arsenal = Arsenal({   
   subsets: ['latin'],
@@ -9,7 +12,15 @@ const arsenal = Arsenal({
 export default function Header() {
   return (
     <header className="w-full flex justify-end py-6 px-4 max-w-[1600px] mx-auto">
-      <h1 className={`${arsenal.className} text-2xl font-bold md:mr-8 mr-4`}>Yeva Yevtikhova</h1>
+      <Link href="/">
+        <Image 
+          src={yevayevtikhova}
+          alt="Yeva Yevtikhova"
+          width={175}
+          height={40}
+          className="md:mr-8 mr-4"
+        />
+      </Link>
     </header>
   );
 } 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
 import pointer from "./images/pointer.png";
+import yeva from "./images/yeva.png";
 import Folder from "./components/Folder";
 
 
 export default function Home() {
   return (
-    <div className="relative flex-col justify-center max-w-[1600px]">
-      <div className="relative flex justify-center w-full mx-auto">
-        <div className="relative w-[700px] h-[600px] flex flex-col p-8 ">
+    <div className="relative w-full flex flex-col items-center justify-center pb-30">
+      <div className="relative flex justify-center w-full">
+        <div className="relative w-[700px] h-[600px] flex flex-col p-8">
           <div className="mt-8">
             <h1 className="text-[93px] font-arsenica italic font-medium">hello<span className="not-italic">!</span></h1>
           </div>
           <div className="mt-[-65px] ml-30">
             <h1 className="text-[93px] font-arsenica font-medium">it&apos;s</h1>
           </div>
-          <div className="mt-[-40px] ml-80">
-            <h1 className="text-[93px] font-arsenica font-medium">Yeva</h1>
+          <div className="mt-[-100px] ml-60">
+            <Image src={yeva} alt="Yeva" width={275} height={93} className="object-contain" />
           </div>
           <div className="w-full flex-col items-center mt-10 ml-40">
             <h1 className="text-[31px] font-arsenica font-medium">let me <span className="italic text-[#42367e] cursor-pointer">introduce</span> myself</h1>
@@ -26,16 +27,16 @@ export default function Home() {
         </div>
       </div>
       <div className="relative flex justify-center w-full">
-        <div className="flex flex-row gap-8 w-full max-w-[1600px] px-8">
-          <div className="flex-3 h-[600px] flex flex-col p-8">
+        <div className="flex flex-row gap-8 w-full max-w-[1200px] px-8 items-center">
+          <div className="flex-3 flex flex-col p-8">
             <p className="text-[20px] font-arsenica italic font-medium">
               <span className="float-left text-[60px] leading-[60px] mr-2 not-italic">P</span>
               <span className="text-[20px]">roud <span className="text-[#42367e]">Ukrainian</span> currently residing in San Francisco, I completed a worldwide journey around 7 countries to pursue education in <span className="text-[#42367e]">Finance & Economics</span>.</span>
             </p>
             <p className="text-[20px] font-arsenica italic mt-6 font-medium">With experience in <span className="text-[#42367e]">business & data analytics</span>, <span className="text-[#42367e]">venture capital</span>, <span className="text-[#42367e]">journalism</span>, and <span className="text-[#42367e]">marketing</span>, I find my purpose in using the technical skills I learned by embedding them into my creative endeavors to craft narratives and support ideas that contribute to <span className="text-[#42367e]">social change</span>.</p>
-            <p className="text-[20px] font-arsenica italic mt-6 font-medium">The most recent manifestation of my purpose became my authorship of <span className="text-[#42367e]">“Undervalued”</span>, a book covering the issue of gender gap in venture capital investment.</p>
+            <p className="text-[20px] font-arsenica italic mt-6 font-medium">The most recent manifestation of my purpose became my authorship of <span className="text-[#42367e]">"Undervalued"</span>, a book covering the issue of gender gap in venture capital investment.</p>
           </div>
-          <div className="flex-2 h-[600px] flex flex-col p-8">
+          <div className="flex-2 flex flex-col p-8">
             <div className="flex flex-row">
               <Folder
                 name="my story"
